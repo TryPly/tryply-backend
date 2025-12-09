0db92d323a2e8af2b157d6636a227a3eb727025b
piersilvio <piersilviomancuso@gmail.com>
2025-12-09 22:41:44 +0100

    Implement audit logging for TryPlyEntity with SecurityContext integration

--- src/main/kotlin/com/tryply/model/entity/TryPlyEntity.kt
+++ src/main/kotlin/com/tryply/model/entity/TryPlyEntity.kt
@@ -1,23 +1,19 @@
 package com.tryply.model.entity
 
+import com.tryply.listener.AuditEntityListener
 import io.quarkus.hibernate.orm.panache.kotlin.PanacheEntity
+import jakarta.persistence.EntityListeners
 import jakarta.persistence.MappedSuperclass
 import jakarta.persistence.PostPersist
 import jakarta.persistence.PrePersist
 
 @MappedSuperclass
+@EntityListeners(AuditEntityListener::class)
 open class TryPlyEntity : PanacheEntity() {
 
     var createdDate : Long = System.currentTimeMillis()
     var lastUpdateDate : Long = System.currentTimeMillis()
+    var createdBy : String = ""
+    var updatedBy : String = ""
 
-    @PrePersist
-    fun setup() {
-        createdDate = System.currentTimeMillis()
-    }
-
-    @PostPersist
-    fun update(){
-        lastUpdateDate = System.currentTimeMillis()
-    }
 }
